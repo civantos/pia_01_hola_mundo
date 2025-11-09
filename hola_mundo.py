28057930d31f92020c233ea90134a6cad60bb4cd
civan <javiercivantos@gmail.com>
2025-11-09 21:06:12 +0100

    Creación de ejemplo Hola Mundo en Python

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+class HolaMundo:
+    def saludar(self):
+        print("Hola Mundo")
+
+# Crear una instancia de la clase y llamar al método
+if __name__ == "__main__":
+    hm = HolaMundo()
+    hm.saludar()
